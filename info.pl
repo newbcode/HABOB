4006bda405783e2ba06e18dc219f0ae87b3f516e
newbcode <newbcode@newb-dtop>
2013-04-17 01:02:00 +0900

    breakfast lunch dinner add

--- info.pl
+++ info.pl
@@ -9,8 +9,7 @@ use HTML::TokeParser::Simple;
 use HTML::TokeParser;
 use HTML::Query qw(Query);
 use Encode;
-#binmode(STDOUT, ":utf8");
-#use encoding 'utf8';
+binmode(STDOUT, ":utf8");
 
 my $login_url = 'http://www.welstory.com/loginAction.do?%2Fmywelstory%2FmywelIndex.jsp&pwd_yn=Y&memId=&pwd=';
 my $base_url = 'http://www.welstory.com/main.jsp';
@@ -24,68 +23,122 @@ $ua->cookie_jar({});
 $response = $ua->get($base_url);
 $response = $ua->get($login_url);
 #$response = $ua->get($week_url);
-$response = $ua->get($week3_url, ":content_file" => "$html");
-#$response = $ua->get($week3_url);
-=pod
+$response = $ua->get($week3_url);
 if ( $response->is_success ) {
-    print $response->decoded_content;
-    #$d_Html = $response->decoded_content;
+    $response = $ua->get($week3_url, ":content_file" => "$html");
 }
 else {
     die $response->status_line;
 }
-#my $p = HTML::TokeParser::Simple->new("welstory.html") or die "Can't Open welstory.html :$!";
-#<td width="52" align="center" valign="middle" class="14 b 72634D" style="border-right:1px solid #E5E0D9; border-bottom:1px solid #E5E0D9>;"
-#my $parser = HTML::TokeParser::Simple->new(\*DATA);
-=cut
 my $day_p = HTML::TokeParser::Simple->new( $html );
 my @days;
+my @seq_days;
+my @course;
+my @foods;
+my @test;
+my ($first_day, $last_day);
+my $c_date = `date "+%Y-%m-%d"`;
 
 while ( my $td = $day_p->get_tag('td') ) {
     if ( my $td_attr = $td->get_attr('width') ) {
-        #my $text = $parser->get_trimmed_text("/td");
-        if ( $td_attr eq '408' ) {
-            push @days, $td_attr;
-            #print $td->as_is;
+        if ( $td_attr == '408' ) {
             my $text = $day_p->get_trimmed_text("/td");
             my $d_text = decode("euc-kr", $text);
-            #my $string = decode("UTF-8", $kor)
-            print "$text\n";
-            print "$d_text\n";
+            ($first_day, $last_day) = $d_text =~ m/.*?(\d+-\d\d-\d\d) ~ (\d+-\d+-\d+)./;
+            push @days, ($first_day);
         }
-        #print "$td_attr\n";
+        elsif ( $td_attr eq '52' ) {
+            if ( my $a_day_attr = $td->get_attr('align') ) {
+                if ( $a_day_attr eq 'center' ) {
+                    my $ss_day = $day_p->get_trimmed_text("/td");
+                    my $d_ss_day = decode("euc-kr", $ss_day);
+                    next if $d_ss_day eq '[IMG]';
+                    push @seq_days, $d_ss_day;
+                }
+            }
+        }
+        elsif ( $td_attr eq '55' ) {
+            if ( my $c_attr = $td->get_attr('align') ) {
+                if ( $c_attr eq 'center' ) {
+                    my $c_day = $day_p->get_trimmed_text("/td");
+                    my $d_c_day = decode("euc-kr", $c_day);
+                    push @course, $d_c_day;
+                }
+            }
+        }
+    }
+    elsif ( my $f_attr = $td->get_attr('height') ) {
+        if ( $f_attr == '16' ) {
+            if ( my $f_c_attr = $td->get_attr('class') ) {
+                if ( $f_c_attr == '11' ) {
+                    my $f_text = $day_p->get_trimmed_text("/td");
+                    my $d_f_text = decode("euc-kr", $f_text);
+                    push @foods, $d_f_text;
+                }
+            }
+        }
     }
 }
-=pod
-my $docName = "welstory.html";
-#my $ddocName = qx |cat $docName|;
-#my $dddocName = Encode::decode("euc-kr", $ddocName);
-#$txt = Encode::decode("euc-kr", $txt);
-my $doc = Query(file => $docName);
-for my $tr ($doc->query("td")) {
-  for my $td (Query($tr)->query("td")) {
-      # $td is now an HTML::Element object for the td element
-          print $td->as_text, "\n";
+
+my @kor_breakfast;
+my @kor_lunch;
+my @kor_dinner;
+my @kor_temp;
+my @ame_breakfast;
+my @ame_lunch;
+my @ame_dinner;
+my @ame_temp;
+my @inter_breakfast;
+my @inter_lunch;
+my @inter_dinner;
+my @inter_temp;
+my @nood_breakfast;
+my @nood_lunch;
+my @nood_dinner;
+my @nood_temp;
+my @inter2_breakfast;
+my @inter2_lunch;
+my @inter2_dinner;
+my @inter2_temp;
+
+my $korean_cnt = 1;
+my $american_cnt = 1;
+my $inter_cnt = 1;
+my $noodle_cnt = 1;
+my $inter2_cnt = 1;
+
+foreach my $today (@seq_days) {
+    print "$today\n";
+    foreach my $today_course (@course) {
+        print "$today_course\n";
+        if ( $today_course eq 'korean' ) {
+            foreach my $today_food (@foods) {
+                if ( $korean_cnt == 1 ) {
+                    push @kor_breakfast, ($today_food);
+                    if ( $today_food eq 'kcal' ) {
+                        $korean_cnt++;
+                    }
+                elsif ( $korean_cnt == 2 ) {
+                    push @kor_lunch, ($today_course, $today_food);
+                    if ( $today_food eq 'kcal' ) {
+                        $korean_cnt++;
+                    }
+                }
+                elsif ( $korean_cnt == 3 ) {
+                    push @kor_dinner, ($today_course, $today_food);
+                    if ( $today_food eq 'kcal' ) {
+                        $korean_cnt++;
+                    }
+                }
+                elsif ( $korean_cnt == 4 ) {
+                    push @kor_temp, ($today_course, $today_food);
+                    if ( $today_food eq 'kcal' ) {
+                        $korean_cnt++;
+                    }
+                }
             }
+        }
+    }
 }
-
-__DATA__
-<table width="100%" border="0" cellspacing="0" cellpadding="0">                                                            
-               <tr>
-                       <td height="16" class="11">ㆍ쇠고기대파해장국</td>
-               </tr>
-               <tr>
-                       <td height="16" class="11">ㆍ비엔나소시지케첩볶음</td>
-               </tr>
-               <tr>
-                       <td height="16" class="11">ㆍ깻잎지절임</td>
-               </tr>
-               <tr>
-                       <td height="16" class="11">ㆍ포기김치</td>
-               </tr>
-               <tr>
-                       <td height="16" class="11">ㆍ쌀밥</td>
-               </tr>
- </table>
-
-
+#print "@kor_breakfast\n";
+#print "@foods\n";
