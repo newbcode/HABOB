5a8fb811e91070be0934c496686b5bff51a8ca37
newbcode <newbcode@debian>
2013-04-19 19:07:00 +0900

    success monday food parser

--- info.pl
+++ info.pl
@@ -112,59 +112,291 @@ my $course_cnt = 1;
 my $kcal_cnt =1;
 my $total_cnt =0;
 
-for (; $course_cnt <= 25; $course_cnt++) {
+
+
+for (; $course_cnt <= 5; $course_cnt++) {
     if ( $course_cnt <= 5  ) {
             foreach my $today_food ( @foods ) {
                 if ( $kcal_cnt == 1 ) {
                     if ( $today_food =~ /kcal$/ ) {
                         push @kor_breakfast, $today_food;
                         $kcal_cnt++;
-            $total_cnt++;
+                        $total_cnt++;
                     }
                     else {
                         push @kor_breakfast, $today_food;
-            $total_cnt++;
+                         $total_cnt++;
                     }
                 }
                 elsif ( $kcal_cnt == 2 ) {
                     if ( $today_food =~ /kcal$/ ) {
                         push @kor_lunch, $today_food;
-            $total_cnt++;
                         $kcal_cnt++;
+                        $total_cnt++;
                     }
                     else {
                         push @kor_lunch, $today_food;
-            $total_cnt++;
+                        $total_cnt++;
                     }
                 }
                 elsif ( $kcal_cnt == 3 ) {
                     if ( $today_food =~ /kcal$/ ) {
                         push @kor_dinner, $today_food;
-            $total_cnt++;
                         $kcal_cnt++;
+                        $total_cnt++;
                     }
                     else {
                         push @kor_dinner, $today_food;
-            $total_cnt++;
+                        $total_cnt++;
                     }
                 }
                 elsif ( $kcal_cnt == 4 ) {
                     if ( $today_food =~ /kcal$/ ) {
                         push @kor_temp, $today_food;
-            $total_cnt++;
                         $kcal_cnt++;
+                        $total_cnt++;
                     }
                     else {
-            $total_cnt++;
                         push @kor_temp, $today_food;
+                        $total_cnt++;
                     }
                 }
             }
-        }
+    }
 }
+splice (@foods, 0, $total_cnt);        
+$total_cnt = 0;
+
+for (; $course_cnt <= 10; $course_cnt++) {
+    if ( $course_cnt >= 6  ) {
+            foreach my $today_food ( @foods ) {
+                if ( $kcal_cnt == 5 ) {
+                    if ( $today_food =~ /kcal$/ ) {
+                        push @ame_breakfast, $today_food;
+                        $kcal_cnt++;
+                        $total_cnt++;
+                    }
+                    else {
+                        push @ame_breakfast, $today_food;
+                         $total_cnt++;
+                    }
+                }
+                elsif ( $kcal_cnt == 6 ) {
+                    if ( $today_food =~ /kcal$/ ) {
+                        push @ame_lunch, $today_food;
+                        $kcal_cnt++;
+                        $total_cnt++;
+                    }
+                    else {
+                        push @ame_lunch, $today_food;
+                         $total_cnt++;
+                    }
+                }
+                elsif ( $kcal_cnt == 7 ) {
+                    if ( $today_food =~ /kcal$/ ) {
+                        push @ame_dinner, $today_food;
+                        $kcal_cnt++;
+                        $total_cnt++;
+                    }
+                    else {
+                        push @ame_dinner, $today_food;
+                         $total_cnt++;
+                    }
+                }
+                elsif ( $kcal_cnt == 8 ) {
+                    if ( $today_food =~ /kcal$/ ) {
+                        push @ame_temp, $today_food;
+                        $kcal_cnt++;
+                        $total_cnt++;
+                    }
+                    else {
+                        push @ame_temp, $today_food;
+                         $total_cnt++;
+                    }
+                }
+            }
+    }
+} 
+
+splice (@foods, 0, $total_cnt);        
+$total_cnt = 0;
+
+for (; $course_cnt <= 15; $course_cnt++) {
+    if ( $course_cnt >= 11 ) {
+            foreach my $today_food ( @foods ) {
+                if ( $kcal_cnt == 9 ) {
+                    if ( $today_food =~ /kcal$/ ) {
+                        push @inter_breakfast, $today_food;
+                        $kcal_cnt++;
+                        $total_cnt++;
+                    }
+                    else {
+                        push @inter_breakfast, $today_food;
+                         $total_cnt++;
+                    }
+                }
+                elsif ( $kcal_cnt == 10 ) {
+                    if ( $today_food =~ /kcal$/ ) {
+                        push @inter_lunch, $today_food;
+                        $kcal_cnt++;
+                        $total_cnt++;
+                    }
+                    else {
+                        push @inter_lunch, $today_food;
+                         $total_cnt++;
+                    }
+                }
+                elsif ( $kcal_cnt == 11 ) {
+                    if ( $today_food =~ /kcal$/ ) {
+                        push @inter_dinner, $today_food;
+                        $kcal_cnt++;
+                        $total_cnt++;
+                    }
+                    else {
+                        push @inter_dinner, $today_food;
+                         $total_cnt++;
+                    }
+                }
+                elsif ( $kcal_cnt == 12 ) {
+                    if ( $today_food =~ /kcal$/ ) {
+                        push @inter_temp, $today_food;
+                        $kcal_cnt++;
+                        $total_cnt++;
+                    }
+                    else {
+                        push @inter_temp, $today_food;
+                         $total_cnt++;
+                    }
+                }
+            }
+    }
+} 
+
+splice (@foods, 0, $total_cnt);        
+$total_cnt = 0;
+
+for (; $course_cnt <= 20; $course_cnt++) {
+    if ( $course_cnt >= 16  ) {
+            foreach my $today_food ( @foods ) {
+                if ( $kcal_cnt == 13 ) {
+                    if ( $today_food =~ /kcal$/ ) {
+                        push @nood_breakfast, $today_food;
+                        $kcal_cnt++;
+                        $total_cnt++;
+                    }
+                    else {
+                        push @nood_breakfast, $today_food;
+                         $total_cnt++;
+                    }
+                }
+                elsif ( $kcal_cnt == 14 ) {
+                    if ( $today_food =~ /kcal$/ ) {
+                        push @nood_lunch, $today_food;
+                        $kcal_cnt++;
+                        $total_cnt++;
+                    }
+                    else {
+                        push @nood_lunch, $today_food;
+                         $total_cnt++;
+                    }
+                }
+                elsif ( $kcal_cnt == 15 ) {
+                    if ( $today_food =~ /kcal$/ ) {
+                        push @nood_dinner, $today_food;
+                        $kcal_cnt++;
+                        $total_cnt++;
+                    }
+                    else {
+                        push @nood_dinner, $today_food;
+                         $total_cnt++;
+                    }
+                }
+                elsif ( $kcal_cnt == 16 ) {
+                    if ( $today_food =~ /kcal$/ ) {
+                        push @nood_temp, $today_food;
+                        $kcal_cnt++;
+                        $total_cnt++;
+                    }
+                    else {
+                        push @nood_temp, $today_food;
+                         $total_cnt++;
+                    }
+                }
+            }
+    }
+} 
+splice (@foods, 0, $total_cnt);        
+$total_cnt = 0;
+
+for (; $course_cnt <= 25; $course_cnt++) {
+    if ( $course_cnt >= 17  ) {
+            foreach my $today_food ( @foods ) {
+                if ( $kcal_cnt == 17 ) {
+                    if ( $today_food =~ /kcal$/ ) {
+                        push @inter2_breakfast, $today_food;
+                        $kcal_cnt++;
+                        $total_cnt++;
+                    }
+                    else {
+                        push @inter2_breakfast, $today_food;
+                         $total_cnt++;
+                    }
+                }
+                elsif ( $kcal_cnt == 18 ) {
+                    if ( $today_food =~ /kcal$/ ) {
+                        push @inter2_lunch, $today_food;
+                        $kcal_cnt++;
+                        $total_cnt++;
+                    }
+                    else {
+                        push @inter2_lunch, $today_food;
+                         $total_cnt++;
+                    }
+                }
+                elsif ( $kcal_cnt == 19 ) {
+                    if ( $today_food =~ /kcal$/ ) {
+                        push @inter2_dinner, $today_food;
+                        $kcal_cnt++;
+                        $total_cnt++;
+                    }
+                    else {
+                        push @inter2_dinner, $today_food;
+                         $total_cnt++;
+                    }
+                }
+                elsif ( $kcal_cnt == 20 ) {
+                    if ( $today_food =~ /kcal$/ ) {
+                        push @inter2_temp, $today_food;
+                        $kcal_cnt++;
+                        $total_cnt++;
+                    }
+                    else {
+                        push @inter2_temp, $today_food;
+                         $total_cnt++;
+                    }
+                }
+            }
+    }
+} 
 
 print "@kor_breakfast\n";
 print "@kor_lunch\n";
 print "@kor_dinner\n";
 print "@kor_temp\n";
-print "$total_cnt\n";
+print "@ame_breakfast\n";
+print "@ame_lunch\n";
+print "@ame_dinner\n";
+print "@ame_temp\n";
+print "@inter_breakfast\n";
+print "@inter_lunch\n";
+print "@inter_dinner\n";
+print "@inter_temp\n";
+print "@nood_breakfast\n";
+print "@nood_lunch\n";
+print "@nood_dinner\n";
+print "@nood_temp\n";
+print "@inter2_breakfast\n";
+print "@inter2_lunch\n";
+print "@inter2_dinner\n";
+print "@inter2_temp\n";
+print "@course\n";
